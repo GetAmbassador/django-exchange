df2805b586b06b0619fc263ddee9c87b3529cdb2
Flavia Missi <flaviamissi@gmail.com>
2016-01-13 13:58:58 +0100

    exchange/utils: check whether commit_unless_managed is available on current django installation

--- exchange/utils.py
+++ exchange/utils.py
@@ -52,9 +52,17 @@ def insert_many(objects, using="default"):
     table = model._meta.db_table
     column_names = ",".join(con.ops.quote_name(f.column) for f in fields)
     placeholders = ",".join(("%s",) * len(fields))
-    con.cursor().executemany("insert into %s (%s) values (%s)"
-                             % (table, column_names, placeholders), parameters)
-    transaction.commit_unless_managed(using=using)
+
+    from django import VERSION
+    # see https://docs.djangoproject.com/en/1.9/internals/deprecation/#deprecation-removed-in-1-8
+    if VERSION[2] < 7:
+        con.cursor().executemany("insert into %s (%s) values (%s)"
+                                % (table, column_names, placeholders), parameters)
+        transaction.commit_unless_managed(using=using)
+    else:
+        with transaction.atomic():
+            con.cursor().executemany("insert into %s (%s) values (%s)"
+                                    % (table, column_names, placeholders), parameters)
 
 
 def update_many(objects, fields=[], using="default"):
@@ -91,11 +99,22 @@ def update_many(objects, fields=[], using="default"):
     table = meta.db_table
     assignments = ",".join(("%s=%%s" % con.ops.quote_name(f.column))
                            for f in fields)
-    con.cursor().executemany("update %s set %s where %s=%%s"
-                             % (table, assignments,
-                                con.ops.quote_name(meta.pk.column)),
-                             parameters)
-    transaction.commit_unless_managed(using=using)
+
+    from django import VERSION
+    # see https://docs.djangoproject.com/en/1.9/internals/deprecation/#deprecation-removed-in-1-8
+    if VERSION[2] < 7:
+        con.cursor().executemany("update %s set %s where %s=%%s"
+                                % (table, assignments,
+                                    con.ops.quote_name(meta.pk.column)),
+                                parameters)
+        transaction.commit_unless_managed(using=using)
+    else:
+        with transaction.atomic():
+            con.cursor().executemany("update %s set %s where %s=%%s"
+                                    % (table, assignments,
+                                        con.ops.quote_name(meta.pk.column)),
+                                    parameters)
+
 
 
 def memoize(ttl=None):
